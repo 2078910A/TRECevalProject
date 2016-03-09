b2601235d4980ccc5bccf72d70eddaad0f74fab1
Blair Aitcheson <baitch1996@gmail.com>
2016-03-09 15:39:28 +0000

    Added enumerated types and created Run model

--- TRECappProject/TREC/models.py
+++ TRECappProject/TREC/models.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import User
 
 
 #A helper function which returns the UNIX path where the user's
-#profile pic is to be stored in the media directory
+#profile pic is to be stored in the media directory.
+#!NOT CURRENTLY USED! (BA 09/03/2016)
 def user_profilepic_directory_path(instance, filename):
     #stores uploaded profile pics to 'media/profile_pics/user_<id>/filename'
     return '/profile_pics/user_{0}/{1}'.format(instance.user.id, filename)
@@ -12,7 +13,6 @@ def user_profilepic_directory_path(instance, filename):
 class UserProfile(models.Model):
 
     user = models.OneToOneField(User)
-    email = models.EmailField()
     profilePic = models.ImageField(upload_to=user_profilepic_directory_path)
     website = models.URLField(blank=True)
     display_name = models.CharField(max_length=50)
@@ -21,32 +21,60 @@ class UserProfile(models.Model):
 
 class Track(models.Model):
 
-    title = models.CharField(max_length=64, unique=True)
+    title = models.CharField(max_length=50)
     track_url = models.URLField(blank=True)
-    description = models.CharField(max_length=256)
+    description = models.CharField(max_length=100)
 
-    #NEWS = 'N'
-    #WEB = 'W'
-    #BLOG = 'B'
-    #MEDICAL = 'M'
-    #LEGAL = 'L'
+    GENRE_CHOICES = (
+            ('NE', 'News'),
+            ('WE', 'Web'),
+            ('ME', 'Medical'),
+            ('BL', 'Blog'),
+            ('LE', 'Legal'),
+        )
 
-    #GENRE_CHOICES = (
-        #(NEWS, 'News'),
-        #(WEB, 'Web'),
-
-        #)
-
-    genre = models.CharField(max_length=2, blank=False)
-
+    genre = models.CharField(max_length=20, choices=GENRE_CHOICES)
 
 class Task(models.Model):
 
     track = models.ForeignKey(Track)
     title = models.CharField(max_length=60)
-    task_url = models.URLField(blank=True)
+    task_url = models.URLField(unique=True)
     description = models.CharField(max_length=256)
     year = models.IntegerField()
 
     #Will store judgement (.qrel) files in 'media/judgements/'
     judgement_file = models.FileField(upload_to='judgements/')
+
+
+class Run(models.Model):
+
+    task = models.ForeignKey(Task)
+    researcher = models.ForeignKey(User)
+    name = models.CharField(max_length=4, unique=True)
+
+    RUN_CHOICES = (
+            ('MA', 'Manual'),
+            ('AU', 'Automatic'),
+        )
+
+    run_type = models.CharField(max_length=2, choices=RUN_CHOICES)
+
+    FEEDBACK_CHOICES = (
+            ('NO', 'None'),
+            ('PS', 'Pseudo'),
+            ('RE', 'Relevance'),
+            ('OT', 'Other'),
+        )
+
+    feedback_type = models.CharField(max_length=2, choices=FEEDBACK_CHOICES)
+
+    QUERY_CHOICES = (
+            ('TI', 'Title'),
+            ('TD', 'Title & Description'),
+            ('DE', 'Description'),
+            ('AL', 'All'),
+            ('OT', 'Other'),
+        )
+
+    query_type = models.CharField(max_length=2, choices=QUERY_CHOICES)
