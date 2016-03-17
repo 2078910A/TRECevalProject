ae689a386aa11ab0f57494465296260b01698579
2085099 <2085099M@student.gla.ac.uk>
2016-03-17 19:14:08 +0000

    Fixed cross-browser bug

--- TRECappProject/TREC/admin.py
+++ TRECappProject/TREC/admin.py
@@ -22,5 +22,7 @@ admin.site.register(Run)
 from TREC.models import UserProfile
 
 admin.site.register(UserProfile)
+
 # Register your models here.
+
 >>>>>>> 778b6a723403237c6fea7a84f08cb4051dd33fce
