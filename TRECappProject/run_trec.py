852f94461a5c096406ef4e76adbdea39fc6eae0a
Sean Tierney <Sean Tierney>
2016-03-22 23:21:20 +0000

    tried again

--- TRECappProject/run_trec.py
+++ TRECappProject/run_trec.py
@@ -1,6 +1,6 @@
 import os
-filename = "data/news/ap.trec.bm.25.0.50.res"
-judgement = "data/news/ap.trec.qrels"
-command = "trec_eval.8.1/trec_eval -c " + judgement + " " + filename
+filename = "~/data/news/ap.trec.bm.25.0.50.res"
+judgement = "~/data/news/ap.trec.qrels"
+command = "~/trec_eval.8.1/trec_eval -c " + judgement + " " + filename
 retvalue = os.popen(command).readlines()
 print retvalue
