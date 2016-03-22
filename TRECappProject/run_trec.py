7898089bcf9041f93ffb0a01443c760d6e59bd81
Sean Tierney <Sean Tierney>
2016-03-22 23:42:09 +0000

    again

--- TRECappProject/run_trec.py
+++ TRECappProject/run_trec.py
@@ -1,5 +1,5 @@
 import os
-os.popen(cd)
+os.popen("cd")
 filename = "~/TRECevalProject/TRECevalProject/TRECappProject/data/news/ap.trec.bm25.0.50.res"
 judgement = "~/TRECevalProject/TRECevalProject/TRECappProject/data/news/ap.trec.qrels"
 command = "~/TRECevalProject/TRECevalProject/TRECappProject/trec_eval.8.1/trec_eval -c " + judgement + " " + filename
