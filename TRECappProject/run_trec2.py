3916cd83a85d5ad7600d0c4145c37813c701c13c
Sean Tierney <Sean Tierney>
2016-03-23 01:18:59 +0000

    Signed-off-by: Sean Tierney <Sean Tierney>

--- TRECappProject/run_trec2.py
+++ TRECappProject/run_trec2.py
@@ -2,7 +2,8 @@ import subprocess
 filename = "~/TRECevalProject/TRECevalProject/TRECappProject/data/news/ap.trec.bm25.0.50.res"
 judgement = "~/TRECevalProject/TRECevalProject/TRECappProject/data/news/ap.trec.qrels"
 command = "~/TRECevalProject/TRECevalProject/TRECappProject/trec_eval.8.1/trec_eval -c " + judgement + " " + filename
-print "command = " + command
+print "command = " + command + "\n"
 output = subprocess.check_output([command],shell=True)
-print type(output) + "\n"
+print type(output) 
+print "\n"
 print output
