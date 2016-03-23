d381bdd393e4df783de7f6f572e00b3705883c77
Sean Tierney <Sean Tierney>
2016-03-23 00:18:57 +0000

    again Signed-off-by: Sean Tierney <Sean Tierney>

--- TRECappProject/run_trec.py
+++ TRECappProject/run_trec.py
@@ -1,7 +1,6 @@
-import commands
+import subprocess as sub
 filename = "~/TRECevalProject/TRECevalProject/TRECappProject/data/news/ap.trec.bm25.0.50.res"
 judgement = "~/TRECevalProject/TRECevalProject/TRECappProject/data/news/ap.trec.qrels"
 command = "~/TRECevalProject/TRECevalProject/TRECappProject/trec_eval -c " + judgement + " " + filename
-print command
-retvalue = commands.getoutput(command)
-print retvalue
+p = sub.Popen(['./trec_eval.8.1/trec_eval -c ' + judgement + " " + filename],stdout=sub.PIPE,stderr=sub.PIPE)
+output, errors = p.communicate()
