c5be62f30cecb3ece304a00f0d4ce73079f92651
Sean Tierney <Sean Tierney>
2016-03-23 01:58:06 +0000

    Signed-off-by: Sean Tierney <Sean Tierney>

--- TRECappProject/run_trec2.py
+++ TRECappProject/run_trec2.py
@@ -14,11 +14,11 @@ for lines in outputList:
 		map = float(line[2])
 	if "P10" in lines:
 		line = lines.split("\t")
-		if line[0] = "P10":
+		if line[0] == "P10":
 			p10 = float(line[2])
 	if "P20" in lines:
 		line = lines.split("\t")
-		if line[0] = "P20":
+		if line[0] == "P20":
 			p20 = float(line[2])
 print type(output) 
 print "\n"
