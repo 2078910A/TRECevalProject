3d5744628562a665847771e749ba5be651187a40
Sean Tierney <Sean Tierney>
2016-03-23 00:05:54 +0000

    again
    Signed-off-by: Sean Tierney <Sean Tierney>

--- TRECappProject/run_trec.py
+++ TRECappProject/run_trec.py
@@ -1,11 +1,11 @@
-import os
-retvalue = os.system("cd ~/TRECevalProject/TRECevalProject/TRECappProject/trec_eval.8.1/")
+import commands
+retvalue = commands.getoutput("cd ~/TRECevalProject/TRECevalProject/TRECappProject/trec_eval.8.1/")
 print retvalue
-retvalue = os.system("make")
+retvalue = commands.getoutput("make")
 print retvalue
 filename = "~/TRECevalProject/TRECevalProject/TRECappProject/data/news/ap.trec.bm25.0.50.res"
 judgement = "~/TRECevalProject/TRECevalProject/TRECappProject/data/news/ap.trec.qrels"
 command = "./trec_eval -c " + judgement + " " + filename
 print command
-retvalue = os.system(command).readlines()
+retvalue = commands.getoutput(command).readlines()
 print retvalue
