07b8f13dd99ef9a4ea7617fc97416e511057d5bd
Sean Tierney <Sean Tierney>
2016-03-24 19:10:36 +0000

    Signed-off-by: Sean Tierney <Sean Tierney>

--- TRECappProject/TREC/views.py
+++ TRECappProject/TREC/views.py
@@ -228,10 +228,10 @@ def submit(request):
             researcher = request.user
             run.task = taskObj
             run.researcher = researcher
-            filename = "~/TRECevalProject/TRECevalProject/TRECappProject/media/runs/" + str(taskObj.slug) + "/"  + str(run.run_file.name)
+            #filename = "~/TRECevalProject/TRECevalProject/TRECappProject/media/runs/" + str(taskObj.slug) + "/"  + str(run.run_file.name)
             print filename
-            #judgement = "~/TRECevalProject/TRECevalProject/TRECappProject/media/judgements/" + str(taskObj.track.title) + "/" + str(taskObj.judgement_file.name)
-            judgement = str(run.run_file)
+            judgement = "~/TRECevalProject/TRECevalProject/TRECappProject/media/judgements/" + str(taskObj.track.title) + "/" + str(taskObj.judgement_file.name)
+            filename = str(run.run_file)
             print judgement
             map = "nothing"
             p10 = "nothing"
