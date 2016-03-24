e47c3938fbf35190d42bbeb741d54b96ee38d90e
Sean Tierney <Sean Tierney>
2016-03-24 17:45:49 +0000

    Signed-off-by: Sean Tierney <Sean Tierney>

--- TRECappProject/TREC/views.py
+++ TRECappProject/TREC/views.py
@@ -219,7 +219,7 @@ def submit(request):
 
     if request.method == 'POST':
         form = SubmitForm(request.POST, request.FILES)
-
+        
         if form.is_valid():
             #name and run file should be saved to run now but we still need to give it a task and researcher
             run = form.save(commit=False)
@@ -234,39 +234,37 @@ def submit(request):
             print judgement
             map = "nothing"
             p10 = "nothing"
-	p20 = "nothing"
-	#filename = "~/TRECevalProject/TRECevalProject/TRECappProject/data/news/ap.trec.bm25.0.50.res"
-	#judgement = "~/TRECevalProject/TRECevalProject/TRECappProject/data/news/ap.trec.qrels"
-	command = "~/TRECevalProject/TRECevalProject/TRECappProject/trec_eval.8.1/trec_eval -c " + judgement + " " + filename
-	print "command = " + command + "\n"
-	output = subprocess.check_output([command],shell=True)
-	outputList = output.split('\n')
-	for lines in outputList:
+            p20 = "nothing"
+            #filename = "~/TRECevalProject/TRECevalProject/TRECappProject/data/news/ap.trec.bm25.0.50.res"
+            #judgement = "~/TRECevalProject/TRECevalProject/TRECappProject/data/news/ap.trec.qrels"
+            command = "~/TRECevalProject/TRECevalProject/TRECappProject/trec_eval.8.1/trec_eval -c " + judgement + " " + filename
+            print "command = " + command + "\n"
+            output = subprocess.check_output([command],shell=True)
+            outputList = output.split('\n')
+            for lines in outputList:
                 if "map" in lines:
                     line = lines.split("\t")
                     map = float(line[2])
                 if "P10" in lines and p10 == "nothing":
-                    
-                    
                     line = lines.split("\t")
                     p10 = float(line[2])
 	        if "P20" in lines and p20 == "nothing":
                     line = lines.split("\t")
                     p20 = float(line[2])
-        print type(output)
-        print "\n"
-        print output
-        print map
-        print "\t"
-        print p10
-        print "\t"
-        print p20
-        run.mean_average_precision = map
-        run.p10 = p10
-        run.p20 = p20
-        run.save()
-        print run.run_file
-        return HttpResponseRedirect('/TRECapp/')
+            print type(output)
+            print "\n"
+            print output
+            print map
+            print "\t"
+            print p10
+            print "\t"
+            print p20
+            run.mean_average_precision = map
+            run.p10 = p10
+            run.p20 = p20
+            run.save()
+            print run.run_file
+            return HttpResponseRedirect('/TRECapp/')
     else:
         form = SubmitForm()
     return render(request, 'TRECapp/submit.html', {'form': form})
