1faf22a34be1c783ba49f62fd6a9e3bf8a16681a
2085099 <2085099M@student.gla.ac.uk>
2016-03-25 21:21:21 +0000

    Population script works

--- TRECappProject/populate_TREC.py
+++ TRECappProject/populate_TREC.py
@@ -10,22 +10,22 @@ import random
 
 def populate():
     user_bob = add_user("Bob", "bob")
-    bobprofile = add_userprofile(user_bob, "https://gla.ac.uk", display_name="bob", organisation="Glasgow Uni")
+    add_userprofile(user=user_bob, display_name="bob", organisation="Glasgow Uni")
     user_steve = add_user("Steve", "steve")
-    steveprofile = add_userprofile(user_steve, "https://gla.ac.uk", display_name="steve", organisation="Stirling Uni")
+    add_userprofile(user_steve, display_name="steve", organisation="Stirling Uni")
     user_david = add_user("David", "david")
-    davidprofile = add_userprofile(user_david, "https://gla.ac.uk", display_name="david", organisation="Falkirk Uni")
+    add_userprofile(user_david, display_name="david", organisation="Falkirk Uni")
     user_john = add_user("John", "john")
-    johnprofile = add_profile(user_john, "https://Si.ac.uk", display_name="john", organisation="Edinburgh")
+    add_userprofile(user_john, display_name="john", organisation="Edinburgh")
 
-    clinicalTrack = add_track("Clinical Decision Support", "https://clinical.com", description="Clinical searching...")
-    federatedWebTrack = add_track("Federated Web Seach", "https://fedWeb.com", description="Search the federated web")
-    kbaTrack = add_track("Knowledge Base Acceleration", "https://kbaweb.com", description="Accelerate Knowledge Base")
+    clinicalTrack = add_track("Clinical Decision Support", "https://clinical.com", description="Clinical searching...", genre="Medical")
+    federatedWebTrack = add_track("Federated Web Seach", "https://fedWeb.com", description="Search the federated web", genre="Web")
+    kbaTrack = add_track("Knowledge Base Acceleration", "https://kbaweb.com", description="Accelerate Knowledge Base",genre="Web")
 
-    task1 = add_task(kbaTrack, "KBA Track 1", "https://twitter.com", "A description of KBA Track 1")
-    task2 = add_task(federatedWedTrack, "FW Track 1", "https://steve.com", "A description of FW Track 1")
-    task3 = add_task(clinicalTrack, "Clinical Track 1", "https://task3.com", "A description of Clinical Track 1")
-    task4 = add_task(kbaTrack, "KBA Track 2", "https://task4.com", "A description of KBA track 2")
+    task1 = add_task(track=kbaTrack, title="KBA Track 1", task_url="https://twitter.com", description="A description of KBA Track 1", year=2005)
+    task2 = add_task(track=federatedWebTrack, title="FW Track 1", task_url="https://steve.com", description="A description of FW Track 1", year=2001)
+    task3 = add_task(track=clinicalTrack, title="Clinical Track 1", task_url="https://task3.com", description="A description of Clinical Track 1", year=2006)
+    task4 = add_task(track=kbaTrack, title="KBA Track 2", task_url="https://task4.com", description="A description of KBA track 2", year=2005)
     #task5 = add_task(federatedWebTrak, "")
 
 def add_user(username, password):
@@ -35,25 +35,23 @@ def add_user(username, password):
     u.save()
     return u
 
-def add_userprofile(user, website, display_name, organisation):
-    up = UserProfile.objects.get_or_create(user=user)
-    up.website = website
+def add_userprofile(user, display_name, organisation, profilePic=None, email="", website="", aboutme=""):
+    up = UserProfile.objects.get_or_create(user=user)[0]
     up.display_name = display_name
     up.organisation = organisation
     up.save()
     return up
 
-def add_track(title, track_url, description):
-    t = Track.objects.get_or_create(title=title)
+def add_track(title, track_url, description, genre):
+    t = Track.objects.get_or_create(title=title)[0]
     t.track_url = track_url
     t.description = description
-    t.genre = random.choice(['NE','WE','ME','BL','LE','OT'])
+    t.genre = genre
     t.save()
     return t
 
 def add_task(track, title, task_url, description, year):
-    t = Task.objects.get_or_create(title=title)
-    t.track = track
+    t = Task.objects.get_or_create(title=title,track=track)[0]
     t.title = title
     t.task_url = task_url
     t.description = description
@@ -62,7 +60,7 @@ def add_task(track, title, task_url, description, year):
     return t
 
 def add_run(task, researcher, name):
-    r = Run.objects.get_or_create(name=name)
+    r = Run.objects.get_or_create(name=name)[0]
     r.researcher = researcher
     r.name = name
     r.run_type = random.choice(['MA','AU'])
